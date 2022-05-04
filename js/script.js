b8c79282f75cb4336b3627400e6d4dcc24a5654f
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-04 16:57:46 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Created by: Jaejun Lee
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict";
+
+function myButtonClicked() {
+  const age = document.getElementById("age").value
+  const day = document.getElementById("days").value 
+
+  
+  if ((day == "tuesday") || (day == "thursday") || (age > 12 && age < 21)) {
+    document.getElementById("answers").innerHTML = "You're eligible for student pricing!."
+  } else {
+    document.getElementById("answers").innerHTML = "You must pay regular price."
+  }
+}
